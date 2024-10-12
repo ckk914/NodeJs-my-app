92abafb26680fac78cc75d9f87080a0a465456ef
KyoungkonCho <50573460+ckk914@users.noreply.github.com>
2024-10-12 22:58:39 +0900

    # express 시작 페이지 main.html로 지정

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 import express from "express";
+import path from "path";
+
+const __dirname = path.resolve();
 
 //express 가져옴
 const app = express();
@@ -6,7 +9,8 @@ const app = express();
 //middleware
 //main page GET
 app.get("/", (req, res) => {
-  res.send("Main Page GET Resquest~!");
+  // res.send("Main Page GET Resquest~!");
+  res.sendFile(__dirname + "/public/main.html");
 });
 app.listen(3000, () => {
   console.log("Server is Running~!🎆");
